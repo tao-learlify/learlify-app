5bb462fe7ec6e88fa6f57d50f5b90f15bc34673c
tgomez epo <tgomezaleixandresandoval.external@epo.org>
2026-05-03 21:00:53 +0200

    chore: reorganise assets — remove all PNGs, consolidate SVGs into illustrations/

---
+++ frontend/src/assets/illustrations/index.ts
@@ -0,0 +1,19 @@
+// Panda mascot
+export { default as Panda } from './pandas/panda.svg'
+export { default as PandaSpeaking } from './pandas/panda-speaking.svg'
+
+// Brand
+export { default as Logo } from './brand/logo.svg'
+
+// Badges
+export { default as BadgeCompleted } from './badges/completed.svg'
+export { default as BadgeDiamond } from './badges/diamond.svg'
+
+// Decorative
+export { default as DecBlueLeft } from './decorative/blue-left.svg'
+export { default as DecBlueRight } from './decorative/blue-right.svg'
+export { default as DecCourses } from './decorative/courses.svg'
+export { default as DecRedLeft } from './decorative/red-left.svg'
+export { default as DecRedRight } from './decorative/red-right.svg'
+export { default as DecTurquoiseLeft } from './decorative/turquoise-left.svg'
+export { default as DecTurquoiseRight } from './decorative/turquoise-right.svg'
